aebce456608376be2a7c15174b53e33f08828267
stacyburris <stacy1burris@gmail.com>
2021-10-25 21:55:34 -0700

    feat: 🎸 practive with copilot

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
-let a;
-console.log(a);
+// let a;
+// console.log(a);
 
-if (a === undefined) {
-  console.log('a is undefined!');
-} else {
-    console.log('a is defined!');
-}
+// if (a === undefined) {
+//   console.log('a is undefined!');
+// } else {
+//     console.log('a is defined!');
+// }
 
-function b(){
-  let myVar;
-  console.log(myVar);
-}
-function a(){
-    let myVar = 2;
-    console.log(myVar);
-    b();
-let myVar = 1;
-console.log(myVar);
-a();
-}
+// function b(){
+//   let myVar;
+//   console.log(myVar);
+// }
+// function a(){
+//     let myVar = 2;
+//     console.log(myVar);
+//     b();
+// let myVar = 1;
+// console.log(myVar);
+// a();
+// }
 
+// long running function
+function waitThreeSeconds() {
+    var ms = 3000 + new Date().getTime();
+    while (new Date() < ms){}
+    console.log('finished function');
+    }
+function clickHandler() {
+    console.log('click event!');
+    }
+    // listen for the click event
+    document.addEventListener('click', clickHandler);
+
+    waitThreeSeconds();
+    console.log('finished execution');
 
